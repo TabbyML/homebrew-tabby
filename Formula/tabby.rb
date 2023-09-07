9bd17b584c278686bf8bbff9f89783b9d8552f97
Meng Zhang <meng@tabbyml.com>
2023-09-07 10:14:56 +0800

    Update tabby.rb

--- Formula/tabby.rb
+++ Formula/tabby.rb
@@ -1,9 +1,14 @@
 class Tabby < Formula
   desc "Tabby: AI Coding Assitatnt"
   homepage "https://github.com/TabbyML/tabby"
-  url "https://github.com/TabbyML/tabby/releases/download/v0.0.1/tabby_aarch64-apple-darwin"
-  sha256 "28d97ae6655d1fe6262321e19bca554643cef1ac6fafaab32a82227a933de42d"
-  version "0.0.1"
+
+  on_macos do
+    on_arm do
+      url "https://github.com/TabbyML/tabby/releases/download/v0.0.1/tabby_aarch64-apple-darwin"
+      sha256 "28d97ae6655d1fe6262321e19bca554643cef1ac6fafaab32a82227a933de42d"
+      version "0.0.1"
+    end
+  end
 
   def install
     bin.install "tabby_aarch64-apple-darwin" => "tabby"
