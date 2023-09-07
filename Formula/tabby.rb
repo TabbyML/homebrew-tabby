6e1f47b9445a7f7c433279b218b4ce06f8927bc6
Meng Zhang <meng@tabbyml.com>
2023-09-07 11:05:40 +0800

    feat: support --head of nightly build

--- Formula/tabby.rb
+++ Formula/tabby.rb
@@ -7,6 +7,8 @@ class Tabby < Formula
       url "https://github.com/TabbyML/tabby/releases/download/v0.0.1/tabby_aarch64-apple-darwin"
       sha256 "28d97ae6655d1fe6262321e19bca554643cef1ac6fafaab32a82227a933de42d"
       version "0.0.1"
+
+      head "https://github.com/TabbyML/tabby/releases/download/nightly/tabby_aarch64-apple-darwin"
     end
   end
 
