5a42b9c9d5d2034ca53d8563dc0a7cbebb95eeef
Meng Zhang <meng@tabbyml.com>
2023-09-13 00:32:54 +0800

    feat: add v0.1.0 release

--- Formula/tabby.rb
+++ Formula/tabby.rb
@@ -4,7 +4,10 @@ class Tabby < Formula
 
   depends_on :macos
   depends_on arch: :arm
-  
+
+  url "https://github.com/TabbyML/tabby/releases/download/v0.1.0/tabby_aarch64-apple-darwin"
+  sha256 "085ef069341f3ebf2018645f0c259cc7162ef220b8e85eb786b37dce731f313c"
+
   head "https://github.com/TabbyML/tabby/releases/download/nightly/tabby_aarch64-apple-darwin"
 
   def install
