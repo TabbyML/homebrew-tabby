8552163e989d19c016540ae276662fcb5be0394d
Meng Zhang <meng@tabbyml.com>
2024-01-29 23:31:18 -0800

    update tabby rc

--- Formula/tabby-rc.rb
+++ Formula/tabby-rc.rb
@@ -5,7 +5,7 @@ class TabbyRc < Formula
   depends_on :macos
   depends_on arch: :arm
 
-  url "https://github.com/TabbyML/tabby/releases/download/v0.7.0-rc.4/tabby_aarch64-apple-darwin"
+  url "https://github.com/TabbyML/tabby/releases/download/v0.8.0-rc.0/tabby_aarch64-apple-darwin"
 
   def install
     bin.install "tabby_aarch64-apple-darwin" => "tabby"
