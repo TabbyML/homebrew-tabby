e7e8060784c58401607fad6d25dead39537e5adf
Meng Zhang <meng@tabbyml.com>
2024-06-06 12:15:44 +0800

    Update tabby.rb

--- Formula/tabby.rb
+++ Formula/tabby.rb
@@ -5,7 +5,7 @@ class Tabby < Formula
   depends_on :macos
   depends_on arch: :arm
 
-  url "https://github.com/TabbyML/tabby/releases/download/v0.11.1/tabby_aarch64-apple-darwin"
+  url "https://github.com/TabbyML/tabby/releases/download/v0.12.0/tabby_aarch64-apple-darwin"
 
   head "https://github.com/TabbyML/tabby/releases/download/nightly/tabby_aarch64-apple-darwin"
 
