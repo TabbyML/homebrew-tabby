cdcac1e5bd14de91dc0b17ecba6b1259a1b9ede1
Meng Zhang <meng@tabbyml.com>
2024-11-07 21:30:05 -0800

    Update tabby.rb for 0.19.0

--- Formula/tabby.rb
+++ Formula/tabby.rb
@@ -2,7 +2,7 @@ class Tabby < Formula
   desc "Tabby: AI Coding Assistant"
   homepage "https://github.com/TabbyML/tabby"
 
-  version "0.18.0"
+  version "0.19.0"
 
   depends_on :macos
   depends_on arch: :arm
