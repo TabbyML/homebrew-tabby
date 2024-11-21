0285693d8ea8a4207b8ceea874e7a290be8f4d1e
liangfung <jueliang@tabbyml.com>
2024-11-21 23:49:27 +0700

    chore: Update tabby-rc.rb

--- Formula/tabby-rc.rb
+++ Formula/tabby-rc.rb
@@ -5,11 +5,11 @@ class TabbyRc < Formula
   depends_on :macos
   depends_on arch: :arm
 
-  url "https://github.com/TabbyML/tabby/releases/download/v0.20.0-rc.2/tabby_aarch64-apple-darwin.zip"
+  url "https://github.com/TabbyML/tabby/releases/download/v0.21.0-rc.1/tabby_aarch64-apple-darwin.tar.gz"
 
   def install
-    bin.install "tabby_aarch64-apple-darwin/tabby" => "tabby"
-    bin.install "tabby_aarch64-apple-darwin/llama-server" => "llama-server"
+    bin.install "tabby" => "tabby"
+    bin.install "llama-server" => "llama-server"
   end
 end
 
