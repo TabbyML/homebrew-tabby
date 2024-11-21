19592cbfe141c1b40ad4113670af6de32c67d425
aliang <jueliang@tabbyml.com>
2024-11-21 23:45:17 +0700

    Revert "chore: Update tabby-rc.rb (#39)" (#40)
    This reverts commit 8293b0fe9f23bb66f9714c5fe46540eb458ae8da.

--- Formula/tabby-rc.rb
+++ Formula/tabby-rc.rb
@@ -5,7 +5,7 @@ class TabbyRc < Formula
   depends_on :macos
   depends_on arch: :arm
 
-  url "https://github.com/TabbyML/tabby/releases/download/v0.21.0-rc.1/tabby_aarch64-apple-darwin.tar.gz"
+  url "https://github.com/TabbyML/tabby/releases/download/v0.20.0-rc.2/tabby_aarch64-apple-darwin.zip"
 
   def install
     bin.install "tabby_aarch64-apple-darwin/tabby" => "tabby"
