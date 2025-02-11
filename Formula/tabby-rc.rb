545dbbb7b4707c79ed603041156816189671112f
Meng Zhang <meng@tabbyml.com>
2025-02-11 15:06:17 +0800

    Update tabby-rc.rb

--- Formula/tabby-rc.rb
+++ Formula/tabby-rc.rb
@@ -2,7 +2,7 @@ class TabbyRc < Formula
   desc "Tabby: AI Coding Assistant"
   homepage "https://github.com/TabbyML/tabby"
 
-  version "0.24.0-rc.4"
+  version "0.25.0-rc.1"
 
   depends_on :macos
   depends_on arch: :arm
