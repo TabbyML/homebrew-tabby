dd3579a866f77ba485082d22265e4e581ffef69b
Wei Zhang <kweizh@tabbyml.com>
2025-09-26 09:28:19 +0800

    chore: release v0.31.2

--- Formula/tabby.rb
+++ Formula/tabby.rb
@@ -2,7 +2,7 @@ class Tabby < Formula
   desc "Tabby: AI Coding Assistant"
   homepage "https://github.com/TabbyML/tabby"
 
-  version "0.31.1"
+  version "0.31.2"
 
   depends_on :macos
   depends_on arch: :arm
